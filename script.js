5d9a5dc8fd9fff7454e66d168ac1c6775ee3825c
Dmytro <csourgg@gmail.com>
2024-04-13 21:46:03 +0300

    task IF / ELSE

---
+++ script.js
@@ -0,0 +1,24 @@
+const number = 5
+
+if(number >= 0) {
+    console.log('positive');
+} else {
+    console.log('negative');
+}
+
+const num1 = 6;
+const num2 = 5;
+
+if(num1 > num2) {
+    console.log(num1 - num2);
+} else {
+    console.log(num1 * num2);
+}
+
+
+const number1 = 21;
+if((number1 % 7 === 0) && (number1 % 3 === 0)) {
+    console.log('you win!');
+} else {
+    console.log('you lose!');
+}
